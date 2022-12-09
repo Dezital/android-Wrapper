1b8e3db57f031343bd84976a0c8fcd134627bfba
developer2 <developer2@dezital.com>
2022-12-09 18:56:35 +0500

    all codes

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+
+export default function App() {
+  return (
+    
+    <WebView 
+    style={styles.container}
+    source={{ uri: 'https://stakingpie.com/login.php' }}
+      
+  />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:20
+  },
+});
